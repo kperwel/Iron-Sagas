d0a7052da0b1d3a7b5cee49d6be6a508dab258e0
Krzysztof Czopek <krzysztofczopek0@gmail.com>
2020-04-30 01:13:11 +0200

    Add direction arrow for toolbar sheet switch

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 import MainScreen from './containers/MainScreen';
 import { Character } from './components/Character';
 import { newBaseCharacter } from './models/CharacterModel';
-import { Drawer, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
-import { Face } from '@material-ui/icons';
+import { Drawer, AppBar, Toolbar, Typography, Divider, Button } from '@material-ui/core';
+import { Face, ArrowForwardIos, ArrowBackIos } from '@material-ui/icons';
 
 export default () => {
   const currentScreen = useSelector(state => state.mainScreens.currentScreen);
@@ -14,9 +14,17 @@ export default () => {
     <div className="App">
       <AppBar>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setOpen(!isOpen)}>
+          <Button
+            startIcon={isOpen && <ArrowBackIos />}
+            aria-label="menu"
+            onClick={() => setOpen(!isOpen)}
+            color="inherit"
+            endIcon={!isOpen && (<ArrowForwardIos />)}
+            variant="outlined"
+          >
             <Face />
-          </IconButton>
+          </Button>
+          <Divider orientation="vertical" flexItem={true} style={{ margin: '0em 1em' }} />
           <Typography variant="h6">
             {currentScreen.toString()}
           </Typography>
