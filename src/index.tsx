24dfaf71fc2700651058e5fe8a3694d2af44e73f
Krzysztof Czopek <krzysztofczopek0@gmail.com>
2020-04-29 02:02:17 +0200

    add redux-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import reducer from './reducers';
 
 import App from './App';
 
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer({}));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
