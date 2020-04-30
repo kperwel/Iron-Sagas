ed3504f9acb9dd9c04ef8cbdab3a5d5241d26586
Krzysztof Czopek <krzysztofczopek0@gmail.com>
2020-04-30 15:32:27 +0200

    improve character sheet

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,34 +1,61 @@
 import * as React from 'react';
 import * as Material from '@material-ui/core';
-import { CharacterModel } from '../models/CharacterModel';
+import { CharacterModel, CharacterStatusType, CharacterStatus, DebilityType } from '../models/CharacterModel';
 
 const Experience = ({ experience }) => (
-  <div>
-    Experience: {
-      [...Array(experience)]
-        .map(_ => '\u25CF')
-        .reduce((a, b) => a + b, '')
-      + '\u25CB'
-      + '| ' + experience
-    }
-  </div>
+  <Material.Card>
+    <Material.CardContent>
+      Experience: {
+        [...Array(experience)]
+          .map(_ => '\u25CF')
+          .reduce((a, b) => a + b, '')
+        + '\u25CB'
+        + '| ' + experience
+      }
+    </Material.CardContent>
+  </Material.Card>
 );
 
 const Stats = ({ stats }) => (
-  <div>
-    [ {stats
-      .map(entry => entry[0] + ':' + entry[1])
-      .join(' | ')
-    } ]
-  </div>
+  <Material.Card>
+    <Material.CardContent>
+      [ {stats
+        .map(entry => entry[0] + ':' + entry[1])
+        .join(' | ')
+      } ]
+    </Material.CardContent>
+  </Material.Card>
+);
+
+const Status = ({ status }) => (
+  <Material.Card>
+    <Material.CardContent>
+      [ {status
+        .map((entry: [CharacterStatusType, CharacterStatus]) => entry[0] + ':' + entry[1].current + '/' + entry[1].max)
+        .join(' | ')
+      } ]
+    </Material.CardContent>
+  </Material.Card>
+);
+
+const Debilities = (a: DebilityType[]) => (
+  <Material.Card>
+    <Material.CardContent>
+      Debilities: {a.length > 0
+        ? a.join(', ')
+        : 'none'}
+    </Material.CardContent>
+  </Material.Card>
 );
 
 const Character = (character: CharacterModel) => (
   <Material.Card>
     <Material.CardHeader title={character.name} />
     <Material.CardContent>
-    <Stats stats={Array.from(character.stats)} />
-    <Experience experience={character.experience} />
+      <Experience experience={character.experience} />
+      <Stats stats={Array.from(character.stats)} />
+      <Status status={Array.from(character.status)} />
+      <Debilities {...character.debilities} />
     </Material.CardContent>
   </Material.Card >
 );
